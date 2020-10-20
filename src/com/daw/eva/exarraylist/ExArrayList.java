173d0960eaac1e34872232c036d840d1bc59b0c0
evaBarbeito <ebarbeito@correu.escoladeltreball.org>
2020-10-20 12:42:25 +0200

    exs arraylist de String i Objecte

--- src/com/daw/eva/exarraylist/ExArrayList.java
+++ src/com/daw/eva/exarraylist/ExArrayList.java
@@ -1,5 +1,7 @@
 package com.daw.eva.exarraylist;
 
+import java.util.ArrayList;
+
 public class ExArrayList {
 
 	public static void main(String[] args) {
@@ -33,6 +35,61 @@ public class ExArrayList {
         System.out.println("array personas indice 2:" + personas[2]);
         System.out.println("array personas indice 3:" + personas[3]);
         //System.out.println("Array personas indice 4:" + personas[4]);
+        
+        
+        
+		/*
+		 * Create an ArrayList named food and add 5 of your favourite food items.
+		 * 
+		 * Traverse the ArrayList using for loop and print all array elements.
+		 * 
+		 * Traverse the ArrayList using for each loop and print all array elements.
+		 */
+        
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("Pizza");
+        food.add("Hamburguesa");
+        food.add("Tarta poma");
+        food.add("Pasta");
+        food.add("Cheesecake");
+
+        for(int i=0;i<food.size();i++){
+           System.out.println(food.get(i));
+        }
+
+        for(String f:food){
+           System.out.println(f);
+        }
+	
+	
+	
+		/*
+		 * Create a student class with following attributes: name, id, percentage
+		 * scored.
+		 * 
+		 * Create parameterised constructors in student class to initialise above
+		 * values.
+		 * 
+		 * Create 4 objects of student class in main.java.
+		 * 
+		 * Create an ArrayList and add the student objects to it.
+		 * 
+		 * Traverse the ArrayList and print all student data.
+		 */
+		Student s1 = new Student("John",1,78.8);
+		Student s2 = new Student("Rob",2,89.6);
+		Student s3 = new Student("Tim",3,90.7);
+		Student s4 = new Student("Mark",4,98.9);
+	
+		ArrayList<Student> s = new ArrayList<Student>();
+		s.add(s1);
+		s.add(s2);
+		s.add(s3);
+		s.add(s4);
+	
+		for(int i=0;i<s.size();i++){
+		   System.out.println(s.get(i).id+" "+s.get(i).getNombre()+" "+s.get(i).percentage);
+		}
 	}
 
 }
---
+++ src/com/daw/eva/exarraylist/Student.java
@@ -0,0 +1,14 @@
+package com.daw.eva.exarraylist;
+
+public class Student extends Persona {
+	 
+	  int id;
+	  double percentage;
+
+	  Student(String name,int id,double percentage){
+	      super(name);
+	      this.id = id;
+	      this.percentage = percentage;
+
+	  }
+}
